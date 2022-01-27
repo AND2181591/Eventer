189266fe2c3ce2082f8b336b216bccb5c677b085
Andrew <AND2181591@maricopa.edu>
2022-01-27 14:48:20 -0700

    Subject: Basic Application and Routing Setup
    Problem: Application development now started and basic setup was needed to get started on main components.
    
    Solution: Added Angular Material for styling and Routing. Signin and signup components now generated.
    
    Notes: This is just a simple jumping off point. Will next need to build the signin and signup components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eventer';
+  
+  onDarkMode() {
+    document.body.classList.toggle('dark-theme')
+  }
 }
